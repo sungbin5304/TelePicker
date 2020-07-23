e67b0357140bdbf87aec69d5261cdf5c84627bf9
sungbin5304 <sungbin.dev@gmail.com>
2020-07-23 16:51:03 +0900

    🎉 first commit 🎉

---
+++ app/src/main/java/com/sungbin/telepicker/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sungbin.telepicker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_telepicker)
+    }
+}
